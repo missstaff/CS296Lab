051e360c53900a6191051bc3397ff1293d3959c1
Shawna Staff <staffs@my.lanecc.edu>
2020-09-30 13:08:28 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Shawna_Staff.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
