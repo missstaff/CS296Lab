0cc83a3df0fd3cd3c5adff1393281da3d226be3f
Shawna Staff <staffs@my.lanecc.edu>
2020-10-28 17:24:54 -0700

    both tests failed then both tests passed :P

---
+++ Models/QuizMV.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shawna_Staff.Models
+{
+    public class QuizMV
+    {
+        public String UserAnswer1 { get; set; }
+        public String RightOrWrong1 { get; set; }
+
+        public String UserAnswer2 { get; set; }
+        public String RightOrWrong2 { get; set; }
+
+        public String UserAnswer3 { get; set; }
+        public String RightOrWrong3 { get; set; }
+
+        public void CheckAnswers()
+        {
+            RightOrWrong1 = UserAnswer1 == "wide" ? "Right" : "Wrong";
+            RightOrWrong2 = UserAnswer2 == "21mm" ? "Right" : "Wrong";
+            RightOrWrong3 = UserAnswer3 == "true" ? "Right" : "Wrong";
+        }
+    }
+}
