0a3d7bb7731bfb087a27164d210f4042833c4201
Shawna Staff <staffs@my.lanecc.edu>
2021-01-15 10:52:43 -0800

    updated AccountController to register a user

--- ShawnaStaffSite/Controllers/AccountController.cs
+++ ShawnaStaffSite/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Shawna_Staff.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,41 @@ namespace Shawna_Staff.Controllers
 {
     public class AccountController : Controller
     {
+        private UserManager<AppUser> userManager;
+        private SignInManager<AppUser> signInManager;
+
+        public AccountController(UserManager<AppUser> userMnger,
+                SignInManager<AppUser> signInmnger)
+        {
+            userManager = userMnger;
+            signInManager = signInmnger;
+        }
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>Register(RegisterVM model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new AppUser { UserName = model.Username };
+                var result = await userManager.CreateAsync(user, model.Password);
+                if(result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach(var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
         }
     }
 }
