91f931f62e1da444bbf2cffc538c733da4aa66e5
Shawna Staff <staffs@my.lanecc.edu>
2021-02-25 20:13:01 -0800

    username shows on webpage in both comments and posts but shows as null in the JSON payload? also my database tables show userId in the forumposts and comments tables and I am unsure how to change that

---
+++ ShawnaStaffSite/Controllers/ForumApiController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shawna_Staff.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shawna_Staff.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ForumApiController : ControllerBase
+    {
+        private readonly ForumContext _context;
+
+        public ForumApiController(ForumContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<ForumApiController>
+        public async Task<ActionResult<IEnumerable<ForumPosts>>> GetPosts()
+        {
+            // converting to a list
+            return await _context.ForumPosts.ToListAsync();
+        }
+
+        // GET api/<ForumApiController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ForumPosts>> GetPost(int id)
+        {
+            var post = await _context.ForumPosts.FindAsync(id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return post;
+        }
+
+        // POST api/<ForumApiController>
+        [HttpPost]
+        public async Task<ActionResult<ForumPosts>> Post(ForumPosts post)
+        {
+            _context.ForumPosts.Add(post);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPost", new { id = post.PostID }, post);
+        }
+
+        // PUT api/<ForumApiController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPost(int id, ForumPosts post)
+        {
+            if (id != post.PostID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(post).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PostExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE api/<ForumApiController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ForumPosts>> DeletePost(int id)
+        {
+            var post = await _context.ForumPosts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            _context.ForumPosts.Remove(post);
+            await _context.SaveChangesAsync();
+
+            return post;
+        }
+
+        private bool PostExists(int id)
+        {
+            return _context.ForumPosts.Any(e => e.PostID == id);
+        }
+    }
+}
---
+++ ShawnaStaffSite/Migrations/20210226022152_UpdatedContext.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shawna_Staff.Migrations
+{
+    public partial class UpdatedContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_AspNetUsers_CommenterId",
+                table: "Comment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_ForumPosts_ForumPostsPostID",
+                table: "Comment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Comment",
+                table: "Comment");
+
+            migrationBuilder.RenameTable(
+                name: "Comment",
+                newName: "Comments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comment_ForumPostsPostID",
+                table: "Comments",
+                newName: "IX_Comments_ForumPostsPostID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comment_CommenterId",
+                table: "Comments",
+                newName: "IX_Comments_CommenterId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments",
+                column: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_AspNetUsers_CommenterId",
+                table: "Comments",
+                column: "CommenterId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_ForumPosts_ForumPostsPostID",
+                table: "Comments",
+                column: "ForumPostsPostID",
+                principalTable: "ForumPosts",
+                principalColumn: "PostID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_AspNetUsers_CommenterId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_ForumPosts_ForumPostsPostID",
+                table: "Comments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments");
+
+            migrationBuilder.RenameTable(
+                name: "Comments",
+                newName: "Comment");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_ForumPostsPostID",
+                table: "Comment",
+                newName: "IX_Comment_ForumPostsPostID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_CommenterId",
+                table: "Comment",
+                newName: "IX_Comment_CommenterId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Comment",
+                table: "Comment",
+                column: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_AspNetUsers_CommenterId",
+                table: "Comment",
+                column: "CommenterId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_ForumPosts_ForumPostsPostID",
+                table: "Comment",
+                column: "ForumPostsPostID",
+                principalTable: "ForumPosts",
+                principalColumn: "PostID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
